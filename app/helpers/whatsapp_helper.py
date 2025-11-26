32024a0747be8e402188771916645cafa2c4751b
Cesar Osimani <cesarosimani@gmail.com>
2025-11-26 02:23:11 -0300

    Eliminé un token hardcodeado de whatsapp api

--- app/helpers/whatsapp_helper.py
+++ app/helpers/whatsapp_helper.py
@@ -165,34 +165,6 @@ def enviar_whatsapp_texto(destinatario: str, mensaje: str) -> dict:
 
 
 
-# ACCESS_TOKEN = "EAAX8UPrP70sBO0nOuasaKei8kWFnXkANHEYQeCqlMG1ly3SnZCgqxmlICbYizCiS6M7UzwSj6ZB0KdRzrYvPXJNUDJs3CO1W2VlU4xHYiVDWEZCTIk1tPVTAy7KYEVzlmsDSBLZAfyW4DAkpo70rrkWivSQ9vGSZAvRyiZCefhcL4AZA2vkWazOGeZC6mNUXEHqXmM6qoUcPJrZBB12gpZA2IgVVi3"
-
-# def enviar_whatsapp(destinatario: str, mensaje: str) -> dict:
-#     """
-#     Envía un mensaje de texto vía WhatsApp usando la API de Meta.
-#     destinatario debe estar en formato internacional (ej. '5493511234567')
-#     """
-#     url = f"{WHATSAPP_API_URL}/{PHONE_NUMBER_ID}/messages"
-#     headers = {
-#         "Authorization": f"Bearer {ACCESS_TOKEN}",
-#         "Content-Type": "application/json"
-#     }
-#     payload = {
-#         "messaging_product": "whatsapp",
-#         # "to": destinatario,
-#         "to": "54351152613442",
-#         "type": "template",
-#         "template": { "name": "hello_world" },
-#         "language": { "code": "en_US" } 
-#     }
-
-#     try:
-#         response = requests.post(url, headers=headers, json=payload)
-#         return response.json()
-#     except Exception as e:
-#         return {"success": False, "error": str(e)}
-
-
 def enviar_whatsapp(destinatario: str, mensaje: str) -> dict:
     url = f"{WHATSAPP_API_URL}/{PHONE_NUMBER_ID}/messages"
     
@@ -229,9 +201,3 @@ def enviar_whatsapp(destinatario: str, mensaje: str) -> dict:
         return {"success": False, "error": str(e)}
 
 
-
-# curl -i -X POST `
-#   https://graph.facebook.com/v22.0/662212736970265/messages `
-#   -H 'Authorization: Bearer <access token>' `
-#   -H 'Content-Type: application/json' `
-#   -d '{ \"messaging_product\": \"whatsapp\", \"to\": \"54351152613442\", \"type\": \"template\", \"template\": { \"name\": \"hello_world\", \"language\": { \"code\": \"en_US\" } } }'
